4f77afcc738f7ba871da24c5d9bb749c11e82160
peper11111 <peper.pleban@gmail.com>
2016-05-14 12:24:16 +0200

    Dodanie zachowania stanu aktywności po przekreceniu ekranu. Dodano zmienna opisująca stan aplikacji.

--- app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/CardioFragment.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/CardioFragment.java
@@ -15,50 +15,30 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.maxwellsport.maxwellsportapp.R;
 
-//TODO: Dodać floating action button po kliku play pojawia sie dwa pause and stop
+//TODO: Automatyczne pobieranie pozycji przez gps, Dodanie google services
 public class CardioFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     private MapView mMapView;
+    private String mStatus;
+    private View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View v = inflater.inflate(R.layout.fragment_cardio, container, false);
-        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mView = inflater.inflate(R.layout.fragment_cardio, container, false);
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
 
-        FloatingActionButton fab_start = (FloatingActionButton) v.findViewById(R.id.fab_start);
-        fab_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                v.findViewById(R.id.start_layout).setVisibility(View.INVISIBLE);
-                v.findViewById(R.id.stop_pause_layout).setVisibility(View.VISIBLE);
-            }
-        });
-
-        FloatingActionButton fab_stop = (FloatingActionButton) v.findViewById(R.id.fab_stop);
-        fab_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                v.findViewById(R.id.stop_pause_layout).setVisibility(View.INVISIBLE);
-                v.findViewById(R.id.start_layout).setVisibility(View.VISIBLE);
-            }
-        });
-
-        return v;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        try {
-            mMap.setMyLocationEnabled(true);
-        } catch (SecurityException e) {
-            e.printStackTrace();
+        if (savedInstanceState != null) {
+            mStatus = savedInstanceState.getString("status");
+            setupFabView();
+        } else {
+            mStatus = "stopped";
         }
-        LatLng latLng = new LatLng(52.221450, 21.006319);
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in PW"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        setupFabListeners();
+
+        return mView;
     }
 
     @Override
@@ -69,25 +49,96 @@ public class CardioFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onPause() {
-        super.onPause();
         mMapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
+        super.onPause();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+        outState.putString("status", mStatus);
         mMapView.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
-        super.onLowMemory();
         mMapView.onLowMemory();
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+
+        LatLng latLng = new LatLng(52.221450, 21.006319);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in PW"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+    }
+
+    private void setupFabListeners() {
+        FloatingActionButton fab_start = (FloatingActionButton) mView.findViewById(R.id.fab_start);
+        fab_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStatus.equals("stopped")) {
+                    mStatus = "running";
+                    setupFabView();
+                }
+            }
+        });
+
+        FloatingActionButton fab_stop = (FloatingActionButton) mView.findViewById(R.id.fab_stop);
+        fab_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStatus.equals("running")) {
+                    mStatus = "stopped";
+                    setupFabView();
+                }
+            }
+        });
+
+        FloatingActionButton fab_pause = (FloatingActionButton) mView.findViewById(R.id.fab_pause);
+        fab_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStatus.equals("running")) {
+                    mStatus = "paused";
+                    setupFabView();
+                } else if (mStatus.equals("paused")) {
+                    mStatus = "running";
+                    setupFabView();
+                }
+            }
+        });
+    }
+
+    private void setupFabView() {
+        FloatingActionButton fab;
+        switch (mStatus) {
+            case "stopped":
+                mView.findViewById(R.id.running_layout).setVisibility(View.INVISIBLE);
+                mView.findViewById(R.id.stopped_layout).setVisibility(View.VISIBLE);
+                break;
+            case "running":
+                mView.findViewById(R.id.stopped_layout).setVisibility(View.INVISIBLE);
+                mView.findViewById(R.id.running_layout).setVisibility(View.VISIBLE);
+                fab = (FloatingActionButton) mView.findViewById(R.id.running_layout).findViewById(R.id.fab_pause);
+                fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_fab_pause));
+                break;
+            case "paused":
+                mView.findViewById(R.id.stopped_layout).setVisibility(View.INVISIBLE);
+                mView.findViewById(R.id.running_layout).setVisibility(View.VISIBLE);
+                fab = (FloatingActionButton) mView.findViewById(R.id.running_layout).findViewById(R.id.fab_pause);
+                fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_fab_start));
+                break;
+        }
     }
 }
