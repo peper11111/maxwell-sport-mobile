5fa4e1fd0323119dc6edd7ee946112b75c272dd6
Kiker <rafalrutyna00@gmail.com>
2016-05-21 17:03:55 +0200

    Zmiana tabel z lokalnymi statystykami w profilu

--- app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/ProfileFragment.java
@@ -1,20 +1,24 @@
 package com.maxwellsport.maxwellsportapp.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
 
 import com.maxwellsport.maxwellsportapp.R;
 
-// TODO zaczać wgl ten profil
 
 public class ProfileFragment extends Fragment {
 
     private Button gymButton;
     private Button cardioButton;
+    private TableLayout cardio;
+    private TableLayout gym;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +26,29 @@ public class ProfileFragment extends Fragment {
 
         gymButton = (Button) v.findViewById(R.id.gymButton);
         cardioButton = (Button) v.findViewById(R.id.cardioButton);
+        cardio = (TableLayout) v.findViewById(R.id.profil_cardio_table);
+        gym = (TableLayout) v.findViewById(R.id.profil_gym_table);
+        gymButton.setBackgroundColor(Color.parseColor("#bebebe"));
+
+        gymButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardio.setVisibility(View.INVISIBLE);
+                gym.setVisibility(View.VISIBLE);
+                gymButton.setBackgroundColor(Color.parseColor("#bebebe"));
+                cardioButton.setBackgroundColor(getResources().getColor(R.color.light_grey));
+            }
+        });
+
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardio.setVisibility(View.VISIBLE);
+                gym.setVisibility(View.INVISIBLE);
+                cardioButton.setBackgroundColor(Color.parseColor("#bebebe"));
+                gymButton.setBackgroundColor(getResources().getColor(R.color.light_grey));
+            }
+        });
 
         return v;
     }
