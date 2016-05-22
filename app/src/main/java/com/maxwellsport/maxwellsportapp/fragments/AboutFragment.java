200d83e0d599f6d8523fe8052eb10597d5e0890d
Kiker <rafalrutyna00@gmail.com>
2016-05-22 21:05:16 +0200

    Dodanie ScrollView do AboutFragment oraz dodanie przejscia do repo po kliknieciu logo GitHuba

--- app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/AboutFragment.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/AboutFragment.java
@@ -1,18 +1,53 @@
 package com.maxwellsport.maxwellsportapp.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.maxwellsport.maxwellsportapp.R;
 
-// TODO zrobic to about
 
-public class AboutFragment extends Fragment{
+public class AboutFragment extends Fragment implements  View.OnClickListener{
+
+    private View view;
+    private ImageView imageView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        imageView = (ImageView) view.findViewById(R.id.github_image_link);
+        imageView.setOnClickListener(this);
+        return view;
+    }
+
+    private boolean isNetworkAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(isNetworkAvailable()){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.setData(Uri.parse("https://github.com/peper11111/MaxwellSportApp"));
+            startActivity(intent);
+        }else {
+            Toast.makeText(getActivity(),"Network not available",Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
