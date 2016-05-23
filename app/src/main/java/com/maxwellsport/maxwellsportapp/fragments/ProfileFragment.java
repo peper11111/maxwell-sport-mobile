6737ebfefbf801f0f0a9fdf57ef03303ae3db986
Kiker <rafalrutyna00@gmail.com>
2016-05-23 17:33:27 +0200

    Zmiana wyglądu profilu, dodanie nazw pol do strings.xml

--- app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/fragments/ProfileFragment.java
@@ -16,40 +16,11 @@ import com.maxwellsport.maxwellsportapp.R;
 
 public class ProfileFragment extends Fragment {
 
-    private Button gymButton;
-    private Button cardioButton;
-    private TableLayout cardio;
-    private TableLayout gym;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        gymButton = (Button) v.findViewById(R.id.gymButton);
-        cardioButton = (Button) v.findViewById(R.id.cardioButton);
-        cardio = (TableLayout) v.findViewById(R.id.profil_cardio_table);
-        gym = (TableLayout) v.findViewById(R.id.profil_gym_table);
-        gymButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
-        gymButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardio.setVisibility(View.INVISIBLE);
-                gym.setVisibility(View.VISIBLE);
-                gymButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                cardioButton.setBackgroundColor(getResources().getColor(R.color.primary_dark_material_light));
-            }
-        });
-
-        cardioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardio.setVisibility(View.VISIBLE);
-                gym.setVisibility(View.INVISIBLE);
-                cardioButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                gymButton.setBackgroundColor(getResources().getColor(R.color.primary_dark_material_light));
-            }
-        });
 
         return v;
     }
