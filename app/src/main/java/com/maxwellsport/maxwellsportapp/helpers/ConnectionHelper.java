1f46e652c222f7da72f436682fbe8a953ad5ad2a
Kiker <rafalrutyna00@gmail.com>
2016-06-19 19:15:46 +0200

    wczytanie adresu, klasa LoginHelper do logowania,

--- app/src/main/java/com/maxwellsport/maxwellsportapp/helpers/ConnectionHelper.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/helpers/ConnectionHelper.java
@@ -3,6 +3,7 @@ package com.maxwellsport.maxwellsportapp.helpers;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -15,12 +16,13 @@ import java.net.URL;
 
 public class ConnectionHelper {
 
-    private String mHost = "http://10.42.0.1:10000";
-    private String mURL = "http://10.42.0.1:10000/api/training/user/1";
+    private String mHost = "http://10.42.0.1";
+    private int mPort;
+    private String mURL = "";
+    private String mTrainingURL = "";
+    private String mRunURL = "";
+    private String mLoginURL = "";
     private String mId;
-    private String mMethod = "GET";
-    private String mInputStream;
-    private String mRequestStringResult;
     private Context mContext;
     private JSONParserHelper mParserService;
 
@@ -30,8 +32,11 @@ public class ConnectionHelper {
         mParserService = new JSONParserHelper(mContext);
     }
 
+
     /* this method download or upload training/stats */
     public void connectToServer(){
+        /* initialize URLs */
+        setConnectionVariables();
         /* check if update or download is needed */
         new DownloadHelper(mContext).execute(mURL);
 //        new UploadHelper(mContext).execute(mURL);
@@ -39,10 +44,9 @@ public class ConnectionHelper {
 
     public String POST(String arg) throws  IOException{
         InputStream inputStream = null;
-        DataOutputStream outputStream = null;
+        DataOutputStream outputStream;
         String result = "";
         String data = "";
-
         try {
             /* create URL */
             URL url = new URL(arg);
@@ -77,7 +81,7 @@ public class ConnectionHelper {
         }
     }
 
-    public static String GET(String arg) throws IOException{
+    public String GET(String arg) throws IOException{
         InputStream inputStream = null;
         String result = "";
         try {
@@ -92,13 +96,19 @@ public class ConnectionHelper {
             urlConnection.setDoInput(true);
             /* connect */
             urlConnection.connect();
-            /* get inputStream */
-            inputStream = urlConnection.getInputStream();
+            int responseCode = urlConnection.getResponseCode();
+            if(responseCode == 404){
+                return Integer.toString(responseCode);
+            }else if(responseCode == 200) {
+                /* get inputStream */
+                inputStream = urlConnection.getInputStream();
             /* convert an input to String */
-            result = convertInputStreamToString(inputStream);
+                result = convertInputStreamToString(inputStream);
             /* disconnect */
-            urlConnection.disconnect();
-            return result;
+                urlConnection.disconnect();
+                return result;
+            }
+            return "";
         }finally {
             if(inputStream != null){
                 inputStream.close();
@@ -122,5 +132,15 @@ public class ConnectionHelper {
     public static boolean isNetworkAvailable(ConnectivityManager connectivityManager) {
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public void setConnectionVariables(){
+        mId = SharedPreferencesHelper.getString(mContext, SharedPreferencesHelper.app_user_id_key, "1");
+        mHost = SharedPreferencesHelper.getString(mContext, SharedPreferencesHelper.settings_server_address_key, "10.42.0.1" );
+        mPort = SharedPreferencesHelper.getInt(mContext, SharedPreferencesHelper.settings_server_port_key, 8081 );
+        mURL = "http://" + mHost + ":" + mPort;
+        mTrainingURL = mURL + "/api/training/user/" + mId;
+        mRunURL = mURL + "/api/run/user/" + mId;
+        mLoginURL = mURL + "api/users/user";
     }
 }
--- app/src/main/java/com/maxwellsport/maxwellsportapp/helpers/LoginHelper.java
+++ app/src/main/java/com/maxwellsport/maxwellsportapp/helpers/LoginHelper.java
@@ -1,20 +1,20 @@
 package com.maxwellsport.maxwellsportapp.helpers;
 
-
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
-import com.maxwellsport.maxwellsportapp.helpers.ConnectionHelper;
-
 import java.io.IOException;
 
-public class DownloadHelper extends AsyncTask<String, Void, String> {
+/**
+ * Created by rafal on 19.06.16.
+ */
+public class LoginHelper extends AsyncTask<String, Void, String> {
 
     private Context mContext;
     private ConnectionHelper mConnectionHelper;
 
-    public DownloadHelper(Context context){
+    public LoginHelper(Context context){
         this.mContext = context;
         mConnectionHelper = new ConnectionHelper(mContext);
     }
@@ -33,9 +33,8 @@ public class DownloadHelper extends AsyncTask<String, Void, String> {
     protected void onPostExecute(String json) {
         /* test message */
         if(json.equals("404")){
-            Toast.makeText(mContext, "B", Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, "Bł", Toast.LENGTH_LONG).show();
         }
-        Toast.makeText(mContext, json, Toast.LENGTH_LONG).show();
 
         /* saving json to SharedPreferences */
 //        JSONParserHelper parserService = new JSONParserHelper(mContext);
